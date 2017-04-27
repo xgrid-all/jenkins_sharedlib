44b1a8e36ffc780dfd7c7a8fa1619d2073f77ab2
muneeb131 <muneeb131@gmail.com>
2017-04-27 10:28:09 +0500

    Update template_seedjob.groovy

--- seed/template_seedjob.groovy
+++ seed/template_seedjob.groovy
@@ -3,17 +3,7 @@
 @Grab(group='org.apache.commons', module='commons-io', version='1.3.2')
 def sout = new StringBuilder(), serr= new StringBuilder()
 
-print "hello there muneeb"
-def scriptDir = getClass().protectionDomain.codeSource.location.path
-
-println(scriptDir)
-
 def cfg = readFileFromWorkspace('seed/authorization.groovy')
-println(cfg)
-
-println "=================="
-//config = new ConfigSlurper().parse(new File('authorization.groovy').toURL())
-//def myconfig = new ConfigSlurper().parse(new File('src/authorization.groovy').toURL())
 def config = new ConfigSlurper().parse(cfg)
 println(config)
 println "==================="
