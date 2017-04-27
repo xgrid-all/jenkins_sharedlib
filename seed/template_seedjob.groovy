f02210f68308f0a5ab77db15635f171fa04bbf9d
muneeb131 <muneeb131@gmail.com>
2017-04-27 12:56:25 +0500

    Update template_seedjob.groovy

--- seed/template_seedjob.groovy
+++ seed/template_seedjob.groovy
@@ -3,14 +3,14 @@
 @Grab(group='org.apache.commons', module='commons-io', version='1.3.2')
 //def sout = new StringBuilder(), serr= new StringBuilder()
 
-println("-----1------")
-def cfg = readFileFromWorkspace('config.ini')
+//println("-----1------")
+def cfg = readFileFromWorkspace('config.cfg')
 def config = new ConfigSlurper().parse(cfg)
 
-println("----- 2 -----")
+//println("----- 2 -----")
 println(config.git_url)
 
-println("printing myurl: ")
+//println("printing myurl: ")
 def myurl = config.git_url + 'ats.git'
 println "==================="
 println(myurl)
