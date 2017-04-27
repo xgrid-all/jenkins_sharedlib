226eecd6ed92d7e90735374df170e71a6e7425a6
muneeb131 <muneeb131@gmail.com>
2017-04-27 10:07:06 +0500

    Update template_seedjob.groovy

--- seed/template_seedjob.groovy
+++ seed/template_seedjob.groovy
@@ -7,75 +7,8 @@ print "hello there muneeb"
 
 println "=================="
 //config = new ConfigSlurper().parse(new File('authorization.groovy').toURL())
-def myconfig = new ConfigSlurper().parse(new File('test/authorization.groovy').toURL())
+def myconfig = new ConfigSlurper().parse(new File('authorization.groovy').toURL())
 println(myconfig)
 println "==================="
 
-def projectRoot = WORKSPACE + "/$PROJECT/"
-def clone = "git clone $PROJECTURL".execute(null, new File(WORKSPACE + "/"))
-clone.consumeProcessOutput(sout, serr)
-clone.waitFor()
-println "out> $sout err> $serr"
 
-folder("$PROJECT")
-{
-    displayName("$PROJECT")
-    description("pipeplines for $PROJECT")
-}
-
-new File("$projectRoot/alfred/pipelines").eachFile()
-{   file->
-    println "project_root :: $projectRoot"
-    println "Pipeline config:"
-    println file.text
-    def config = new ConfigSlurper().parse(file.text)
-    
-
-    // Creating workload based folders inside the repo folder
-    folder("$PROJECT/$config.job.workload")
-    {
-        displayName("$config.job.workload")
-        description("Pipeplines for $config.job.workload")
-    }
-
-    // Creating Pipeline
-    pipelineJob("$PROJECT/$config.job.workload/$config.job.name")
-    {
-        logRotator(30,-1,-1,-1)
-        if( config.job.pipeline_type == "periodic" )
-        {
-            definition
-            {
-                cpsScm
-                {
-                    label(config.job.label)
-                    scm
-                    {
-                        git
-                        {
-                            branch(BRANCH)
-                            remote
-                            {
-                                credentials('jenkins')
-                                url(PROJECTURL)
-                            }
-                        }
-                    }
-                    scriptPath("alfred/pipelines/" + org.apache.commons.io.FilenameUtils.getBaseName(file.name))
-                }
-            }
-            triggers
-            {
-                cron(config.job.schedule)
-            }
-            publishers
-            {
-                wsCleanup()
-                archiveArtifacts
-                {
-                    pattern(config.job.artifacts)
-                }
-            }
-        }
-    }
-}
