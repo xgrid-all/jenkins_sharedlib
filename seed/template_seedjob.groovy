c900587e3c5e03585d9e7444a5e29e014fe13d3e
muneeb131 <muneeb131@gmail.com>
2017-04-27 11:28:58 +0500

    Update template_seedjob.groovy

--- seed/template_seedjob.groovy
+++ seed/template_seedjob.groovy
@@ -1,7 +1,7 @@
 #!groovy
 
 @Grab(group='org.apache.commons', module='commons-io', version='1.3.2')
-def sout = new StringBuilder(), serr= new StringBuilder()
+//def sout = new StringBuilder(), serr= new StringBuilder()
 
 def cfg = readFileFromWorkspace('config.cfg')
 def config = new ConfigSlurper().parse(cfg)
