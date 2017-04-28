02a9bb3e927c7e9c413a0b51108ada3a071a626e
muneeb131 <muneeb131@gmail.com>
2017-04-28 11:09:25 +0500

    Update job.groovy

--- vars/job.groovy
+++ vars/job.groovy
@@ -10,7 +10,7 @@ def call(body) {
   println "========== Loading configuration ======="
   // Load default configurations
   @Grab(group='org.apache.commons', module='commons-io', version='1.3.2')
-  def cfg_file = readFile('config.ini')
+  def cfg_file = readFile 'config.ini'
   def config = new ConfigSlurper().parse(cfg_file)
   
   // Loading jenkins jenkinsLibrary
